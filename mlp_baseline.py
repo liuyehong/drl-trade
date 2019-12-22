a3a5cf58ca9df4b1f11be5d84e92e3484193b7e6
Liu Yehong <liuyehong@users.noreply.github.com>
2019-12-23 00:37:29 +0800

    Add files via upload

--- mlp_baseline.py
+++ mlp_baseline.py
@@ -12,17 +12,18 @@ from stable_baselines import PPO2
 # The algorithms require a vectorized environment to run
 env = DummyVecEnv([lambda: QtradeEnv()])
 
-model = PPO2(MlpPolicy, env, verbose=0)
-model.learn(total_timesteps=QtradeEnv().total_steps*20)
-model.save('ppo2')
+
+model = PPO2(MlpPolicy, env, verbose=0, tensorboard_log= "./ppo2_mlp_tensorboard/")
+model.learn(total_timesteps=QtradeEnv().total_steps*100, tb_log_name="first_run")
+model.save('./save/ppo2')
 del model
 
-model = PPO2.load('ppo2')
+model = PPO2.load('./save/ppo2')
 
 
 obs = env.reset()
 print('---Test begins---')
-for i in range(QtradeEnv().total_steps*10):
+for i in range(QtradeEnv().total_steps*50):
     action, _states = model.predict(obs)
     obs, rewards, done, info = env.step(action)
 
